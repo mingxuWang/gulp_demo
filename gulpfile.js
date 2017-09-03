448d99dd19f115f944eaf66958a005e91e3292d8
Jiu <jiu@jiudegushi.com>
2017-09-03 22:42:11 +0800

    add

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp"),
+    less = require("gulp-less");
+gulp.task('testLess', function () {
+    gulp.src('src/less/index.less') //该任务针对的文件
+        .pipe(less()) //该任务调用的模块
+        .pipe(gulp.dest('src/css')); //将会在src/css下生成index.css
+});
+gulp.task('default',function(){
+
+});
